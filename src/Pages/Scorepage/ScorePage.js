aa4b083b5bc0a12e6bce3265a9b519768d3f03cb
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2022-05-25 00:25:55 +0530

    add: lottie file

--- src/Pages/Scorepage/ScorePage.js
+++ src/Pages/Scorepage/ScorePage.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { Navbar } from '../../Components/Navbar/Navbar'
 import './scorepage.css'
+import Lottie from 'react-lottie';
+import animationData from '../../Assets/lotties/confetti.json';
 
 const ScorePage = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
     return (
         <div>
             <Navbar />
             {/* <lottie-player className="result-gif" src="https://assets4.lottiefiles.com/packages/lf20_qpxqycif.json"
         background="transparent" speed="2" autoplay>
     </lottie-player> */}
+            <Lottie
+                options={defaultOptions}
+                height={400}
+                width={400}
+            />
             <div className="result-container center-items light-txt">
                 <h1 className="lg-title new-font gradient-font">YOUR SCORE IS 5</h1>
                 <div className="stats-container">
