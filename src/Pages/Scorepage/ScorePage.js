dbc87cccaac505aa62fe664ec6ce14848baf2ab4
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2022-05-28 09:33:22 +0530

    feat: share score

--- src/Pages/Scorepage/ScorePage.js
+++ src/Pages/Scorepage/ScorePage.js
@@ -1,41 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navbar } from '../../Components/Navbar/Navbar'
 import './scorepage.css'
 import Lottie from 'react-lottie';
-import animationData from '../../Assets/lotties/confetti.json';
+import animationData from '../../Assets/lotties/game.json';
+import { Link } from 'react-router-dom'
+import { useQuiz } from '../../Context/quiz-context'
+import { WhatsappShareButton, WhatsappIcon } from 'react-share'
 
 const ScorePage = () => {
     const defaultOptions = {
         loop: true,
+        speed: 1,
         autoplay: true,
         animationData: animationData,
         rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
+            preserveAspectRatio: "xMidYMid meet"
         }
     };
+    const { quizState, quizDispatch } = useQuiz()
+    const quizTitle = quizState.selectedQuiz.split(':')[1].toUpperCase()
+    const currentURL = window.location.href
+
+    useEffect(() => {
+        return () => {
+            quizDispatch({ type: 'CLEAR_DATA' })
+        }
+    }, [])
+
     return (
-        <div>
+        <div className="result-page">
             <Navbar />
-            {/* <lottie-player className="result-gif" src="https://assets4.lottiefiles.com/packages/lf20_qpxqycif.json"
-        background="transparent" speed="2" autoplay>
-    </lottie-player> */}
-            <Lottie
-                options={defaultOptions}
-                height={400}
-                width={400}
-            />
-            <div className="result-container center-items light-txt">
-                <h1 className="lg-title new-font gradient-font">YOUR SCORE IS 5</h1>
-                <div className="stats-container">
-                    <p className="md-title new-font center-txt light-grey-txt">Valorant Quiz Stats </p>
-                    <p className="md-title mg-t-10 stat">Correct answers <span className="green-txt">3</span></p>
-                    <p className="md-title stat">Wrong answers <span className="red-txt">2</span> </p>
-                </div>
-                <a href="../index.html" className="mg-t-20 light-grey-txt">Go Back to Home</a>
+            <div className="result-gif">
+                <Lottie
+                    options={defaultOptions}
+                    height={450}
+                    width={450}
+                />
             </div>
-            <button className="fab pos-abs bottom-left">
-                <i className="fa fa-solid fa-share"></i>
-            </button>
+            <div className="result-container center-items light-txt">
+                <h1 className="lg-title new-font gradient-font">YOUR SCORE IS {quizState.quizScore.score}</h1>
+                <div className="stats-container">
+                    <p className="md-title new-font center-txt light-grey-txt">{quizTitle}</p>
+                    <p className="md-title mg-t-10 stat">Correct answers <span className="green-txt">{quizState.quizScore.score}</span></p>
+                    <p className="md-title stat">Wrong answers <span className="red-txt">{quizState.quizScore.wrongAns}</span> </p>
+                </div>
+                <Link to="/" className="mg-t-20 light-grey-txt">Go Back to Home</Link>
+                <div className='pos-abs fab bottom-right'>
+                    <WhatsappShareButton url={currentURL} title={`Hey checkout my score for the ${quizTitle}`}>
+                        <WhatsappIcon size={37} iconFillColor="white" round={true}></WhatsappIcon>
+                    </WhatsappShareButton>
+                </div>
+            </div>
         </div>
     )
 }
